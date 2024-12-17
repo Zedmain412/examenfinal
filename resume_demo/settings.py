abe9b2ba1ef75f88ff2c992a5da6ef21683a42c5
ArielR <zedmain412@gmail.com>
2024-12-17 10:34:25 -0500

    updated

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = "resume_demo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_project",  #
+        "USER": "root",  
+        "PASSWORD": "bananacat123", 
+        "HOST": "127.0.0.1",  
+        "PORT": "3306",  
     }
 }
+
+
 
 
 # Password validation
